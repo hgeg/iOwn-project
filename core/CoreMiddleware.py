8c5ab4160b25d5d66cf8bf3b10b9d8a0bd0f59fa
Can Bülbül <alicanblbl@gmail.com>
2012-11-26 23:45:38 +0200

    last seen feature

--- core/CoreMiddleware.py
+++ core/CoreMiddleware.py
@@ -1,2 +1,11 @@
+from core.models import *
+
 class CoreMiddleware:
-def process_view(request):
+    def process_request(self, request):
+        try:
+            myUser = request.user
+            me = Person.objects.get(user = myUser.pk)
+            print "user name:",me.name
+            me.save()
+        except: pass
+        return None
