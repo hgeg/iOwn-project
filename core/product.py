4c9b545406977016ab8fdcbe3c6ec18407a3dba4
Can Bülbül <alicanblbl@gmail.com>
2012-12-26 00:52:06 +0200

    thumbnail fix

--- core/product.py
+++ core/product.py
@@ -3,7 +3,7 @@ import urllib2,json
 key = 'AIzaSyC7BnxpiiFqTV6GLyztM0qQ0pvlgDlDQdQ'
 
 def query(q):
-  url = "https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=%s&restrictBy=condition:new&thumbnails=*:200&alt=json"%(key,q)
+  url = "https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=%s&restrictBy=condition:new&thumbnails=220:*&alt=json"%(key,q)
   data = json.loads(urllib2.urlopen(url).read())
   #print data
   try:
