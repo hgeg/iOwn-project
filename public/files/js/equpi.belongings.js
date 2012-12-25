118c6f7a50779f9cfe67b5a2e2ede1f5b59253b8
Oguz Bayraktar <fayt@cuthalion.com>
2012-12-25 23:51:59 +0200

    profile page update

---
+++ public/files/js/equpi.belongings.js
@@ -0,0 +1,17 @@
+$.hideDetails = function() {
+	$(".equpi-belongingsDetails-wrapper").hide();
+	$(".equpi-belongingsElement").css("float","left");
+	$(".equpi-belongingsElement").css("width","100%");
+	$(".equpi-belongingsElement-image").css("width", "100%");
+};
+
+
+$(document).ready(function() {
+	$(".equpi-belongingsDetails-wrapper").hide();
+	$(".equpi-belongingsElement").css("float","left");
+	$(".equpi-belongingsElement").css("width","100%");
+	$(".equpi-belongingsElement-image").css("width", "90%");
+	
+});
+
+
