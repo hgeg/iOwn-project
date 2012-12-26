4a3dfbd2e77a979fc9dc3e63d7839ae2e93ae68b
Oguz Bayraktar <fayt@cuthalion.com>
2012-12-26 20:02:27 +0200

    hover update, minor profile updates, color fail

--- public/files/js/equpi.belongings.js
+++ public/files/js/equpi.belongings.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-	$(".equpi-belongingsElement-lovebox").hide();
+	$(".equpi-belongingsElement-detailsbox").animate({
+		height: 'toggle'
+	}, 0);
 	
 	$(".equpi-belongingsElement").hover(function() {
-		$(this).children(".equpi-belongingsElement-lovebox").height($(this).width() * 0.1);
-		$(this).children(".equpi-belongingsElement-lovebox").css("font", 
-			(""+($(this).children(".equpi-belongingsElement-lovebox").height() / 1.5)+" sans-serif"));
-		$(this).children(".equpi-belongingsElement-lovebox").css("top", 
-			(($(this).height() * 0.04) + 7));
-  	$(this).addClass("equpi-belonging-hover");
-		$(this).children(".equpi-belongingsElement-lovebox").show();
+		$(this).addClass("equpi-belonging-hover");
+		$(this).children(".equpi-belongingsElement-detailsbox").animate({
+			height: 'toggle'
+		}, 100);
 	}, function() {
-  	$(this).removeClass("equpi-belonging-hover");
-  	$(this).children(".equpi-belongingsElement-lovebox").hide();
-	});	
+		$(this).removeClass("equpi-belonging-hover");
+		$(this).children(".equpi-belongingsElement-detailsbox").animate({
+			height: 'toggle'
+		}, 100);
+		
+	});
 });
 
 
