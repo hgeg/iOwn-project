c57d8dae92d95a3403ab523e659797f8e543899f
Oguz Bayraktar <fayt@cuthalion.com>
2012-12-29 01:05:29 +0200

    animation bug fix

--- public/files/js/equpi.belongings.js
+++ public/files/js/equpi.belongings.js
@@ -1,59 +1,62 @@
 $(document).ready(function(){
+	var selectedElement = null;
 	$(".equpi-belongingsElement-detailsbox").hide(0);
 	$(".equpi-belongingsElement-controlCol").hide(0);
 	
 	$(".equpi-belongingsElement").click(function() {
-		var selectedElement = $(this);
-		var selectedElementControlCol = $(this).children(".equpi-belongingsElement-controlCol");
-		var selectedElementMainCol = $(this).children(".equpi-belongingsElement-mainCol");
-		var selectedCategory = $(this).parent();
-		var otherElementsInSelectedCategory = $(this).siblings();
+		if (selectedElement == null || $(this).get(0) != selectedElement.get(0)) {
+			selectedElement = $(this);
+			var selectedElementControlCol = $(this).children(".equpi-belongingsElement-controlCol");
+			var selectedElementMainCol = $(this).children(".equpi-belongingsElement-mainCol");
+			var selectedCategory = $(this).parent();
+			var otherElementsInSelectedCategory = $(this).siblings();
 
-		var otherCategories = $(".equpi-belongingsCategory").not(selectedCategory);
-		var otherElementsInOtherCategories = otherCategories.children();
+			var otherCategories = $(".equpi-belongingsCategory").not(selectedCategory);
+			var otherElementsInOtherCategories = otherCategories.children();
 
-		//Shrink all other Categories
-		otherCategories.animate({
-			width: '14%'
-		}, 200);
-		//Grow the selected Category 
-		selectedCategory.animate({
-			width: '20%'
-		}, 200);
-		//Shrink and Grow Elements
-		selectedElement.animate({
-			width: '96%'
-		}, 200);
-		otherElementsInSelectedCategory.animate({
-			width: '72%'
-		}, 200);
-		otherElementsInOtherCategories.animate({
-			width: '96%'
-		}, 200);
+			//Shrink all other Categories
+			otherCategories.animate({
+				width: '14%'
+			}, 200);
+			//Grow the selected Category 
+			selectedCategory.animate({
+				width: '20%'
+			}, 200);
+			//Shrink and Grow Elements
+			selectedElement.animate({
+				width: '96%'
+			}, 200);
+			otherElementsInSelectedCategory.animate({
+				width: '72%'
+			}, 200);
+			otherElementsInOtherCategories.animate({
+				width: '96%'
+			}, 200);
 
-		//Shrink and Grow MainCol
-		selectedElementMainCol.animate({
-			width: '75%'
-		}, 200);
-		$(".equpi-belongingsElement-mainCol").not(selectedElementMainCol).animate({
-			width: '100%'
-		}, 200);
+			//Shrink and Grow MainCol
+			selectedElementMainCol.animate({
+				width: '75%'
+			}, 200);
+			$(".equpi-belongingsElement-mainCol").not(selectedElementMainCol).animate({
+				width: '100%'
+			}, 200);
 
-		//Hide and Show ControlCol
-		selectedElementControlCol.show(200);
-		$(".equpi-belongingsElement-controlCol").not(selectedElementControlCol).hide(200);
+			//Hide and Show ControlCol
+			selectedElementControlCol.show(200);
+			$(".equpi-belongingsElement-controlCol").not(selectedElementControlCol).hide(200);
 
-		
-		selectedElement.addClass("equpi-belonging-hover");
-		otherElementsInSelectedCategory.removeClass("equpi-belonging-hover");
-		otherElementsInOtherCategories.removeClass("equpi-belonging-hover");
-
-
+			
+			selectedElement.addClass("equpi-belonging-hover");
+			otherElementsInSelectedCategory.removeClass("equpi-belonging-hover");
+			otherElementsInOtherCategories.removeClass("equpi-belonging-hover");
 
 
-		selectedElementMainCol.children(".equpi-belongingsElement-detailsbox").show(200);
-		otherElementsInSelectedCategory.children(".equpi-belongingsElement-mainCol").children(".equpi-belongingsElement-detailsbox").hide(200);
-		otherElementsInOtherCategories.children(".equpi-belongingsElement-mainCol").children(".equpi-belongingsElement-detailsbox").hide(200);
+
+
+			selectedElementMainCol.children(".equpi-belongingsElement-detailsbox").show(200);
+			otherElementsInSelectedCategory.children(".equpi-belongingsElement-mainCol").children(".equpi-belongingsElement-detailsbox").hide(200);
+			otherElementsInOtherCategories.children(".equpi-belongingsElement-mainCol").children(".equpi-belongingsElement-detailsbox").hide(200);
+		}
 	});
 });
 
